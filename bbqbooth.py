f9b2058c1437bbc9ef02fbebe03505d89cb375b1
Dirk Uys <dirkcuys@gmail.com>
2018-05-16 22:54:31 +0000

    update booth for bbq

--- bbqbooth.py
+++ bbqbooth.py
@@ -2,6 +2,7 @@ from jam_picamera import JamPiCamera
 from auth import CON_KEY, CON_SEC, ACC_TOK, ACC_SEC
 from gpiozero import Button
 from time import sleep
+from PIL import Image
 import logging
 
 logger = logging.getLogger('photobooth')
@@ -12,7 +13,7 @@ camera = JamPiCamera()
 button = Button(14, hold_time=5)
 camera.resolution = (1024, 768)
 camera.start_preview()
-camera.vflip = True
+camera.hflip = True
 camera.annotate_text_size = 70
 
 def quit():
@@ -28,17 +29,42 @@ def countdown(n):
 
 button.when_held = quit
 
+def _pad(resolution, width=32, height=16):
+    # A little utility routine which pads the specified resolution
+    # up to the nearest multiple of *width* and *height*; this is
+    # needed because overlays require padding to the camera's
+    # block size (32x16)
+    return (
+        ((resolution[0] + (width - 1)) // width) * width,
+        ((resolution[1] + (height - 1)) // height) * height,
+    )
+
+photo = None
+
 while True:
-    camera.annotate_text = "Press button to start"
-    logger.info("waiting for button press")
-    button.wait_for_press()
-    logger.info("button pressed")
+    if photo:
+        #camera.annotate_text = "Press button to take your photo"
+        #logger.info("waiting for button press")
+	overlay_image = Image.open(photo).convert('RGBA')
+	pad = Image.new('RGB', _pad(camera.resolution))
+        pad.paste(overlay_image, (0, 0))
+        #overlay = camera.add_overlay(pad.tobytes(), alpha=100, layer=3)
+        overlay = camera.add_overlay(pad.tobytes())
+        camera.preview.alpha = 0
+
+        #overlay = camera.add_overlay(overlay_image.tobytes())
+        #button.wait_for_press()
+        #logger.info("button pressed")
+        sleep(5)
+        camera.remove_overlay(overlay)
+        camera.preview.alpha = 255
+        
     camera.annotate_text = "Press button to take photo"
     button.wait_for_press()
     logger.info("button pressed")
     button.wait_for_release()
     logger.info("button released")
-    sleep(1)
+    #sleep(1)
     countdown(3)
     logger.info("capturing photo")
     photo = camera.capture()
